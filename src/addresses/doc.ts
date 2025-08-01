5e1c91211d494fa5bb53181857b75ff4c596d096
Bhimgouda D Patil <satvikp508@gmail.com>
2025-08-01 21:51:32 +0530

    supported cUSDO

--- src/addresses/doc.ts
+++ src/addresses/doc.ts
@@ -18,3 +18,20 @@
 //   },
 //   "pendleMarket": "0xE467c5C54748f0125d729280F90DeD77C4e720a4"
 // }
+
+// "0xBC6736d346a5eBC0dEbc997397912CD9b8FAe10a": {
+//   "address": "0xBC6736d346a5eBC0dEbc997397912CD9b8FAe10a",
+//   "decimals": 18,
+//   "name": "PT-USDe-25SEP2025",
+//   "symbol": "PT-USDe",
+//   "image": "https://storage.googleapis.com/prod-pendle-bucket-a/images/uploads/d268798b-a558-4ebe-9b62-eab24a82ce02.svg",
+//   "loanToken": {
+//     "address": "0xC139190F447e929f090Edeb554D95AbB8b18aC1C",
+//     "name": "USDtb",
+//     "symbol": "USDtb",
+//     "decimals": 18,
+//     "valueInUsd": 1
+//   },
+//   "morphoMarketId": "0x3462a9442ec1aa46fc38bf91472b593b8fb86bf18eac47ebf82493dc4bed5b1b",
+//   "pendleMarket": "0x6d98a2b6CDbF44939362a3E99793339Ba2016aF4"
+// }
