2e2de8720c39379a1833f635f54bb4f36b4ce2e2
AMOGHPTL <amoghp549@gmail.com>
2025-10-23 00:52:13 +0530

    max leverage and available to borrow

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import { CollateralToken } from "../types";
+import BigNumber from "bignumber.js";
+import { formatNumber } from "../utils/formatNumber";
 
 const ProductCard = ({
   collateralToken,
 }: {
-  collateralToken: CollateralToken
+  collateralToken: CollateralToken;
 }) => {
   const navigate = useNavigate();
 
@@ -20,45 +22,81 @@ const ProductCard = ({
           <div className="flex justify-between items-center pb-[16px] border-b-[1px] border-white border-opacity-[6%]">
             <div className="flex items-center gap-2">
               <div>
-                <img src={`/tokens/${collateralToken.symbolExtended}.svg`} alt="" className="w-[28px]" />
+                <img
+                  src={`/tokens/${collateralToken.symbolExtended}.svg`}
+                  alt=""
+                  className="w-[28px]"
+                />
               </div>
               <div className="flex flex-col gap-[4px]">
                 <div className="flex gap-[8px] items-center">
-                  <p className="text-[16px] font-medium">{collateralToken.symbol.split("-")[1]}</p>
+                  <p className="text-[16px] font-medium">
+                    {collateralToken.symbol.split("-")[1]}
+                  </p>
                   {/* <img src={infoIcon} alt="" className="w-[16px]" /> */}
                 </div>
                 <div className="flex items-center text-[13.4px] text-[#CCCCCC]">
                   <p>
-                    {collateralToken.maturityDate} ({collateralToken.maturityDaysLeft} days)
+                    {collateralToken.maturityDate} (
+                    {collateralToken.maturityDaysLeft} days)
                   </p>
                   <p></p>
                 </div>
               </div>
             </div>
             <div className="flex gap-1">
-              <p className="text-[20px] text-[#68EA6A]">{collateralToken.defaultLeverageApy}%</p>
+              <p className="text-[20px] text-[#68EA6A]">
+                {collateralToken.defaultLeverageApy}%
+              </p>
               <p className="hidden group-hover:flex text-[20px] text-[#68EA6A]">
                 APY
               </p>
             </div>
           </div>
           {/* second part */}
-          <div className="flex justify-between items-center">
-            <div>
-              <p className="text-[12px] text-[#CCCCCC]">Yield source</p>
-              <p className="text-[16px] text-[#E4E4E4] font-normal">
-                {collateralToken.info.yieldSource}
-              </p>
-            </div>
-            <div>
-              <p className="text-[12px] text-right text-[#CCCCCC]">
-                Underlying collateral
-              </p>
-              <div className="flex text-right justify-end items-center gap-[2px]">
-                {/* <img src={underlyingCollatateralIcon} alt="" className="w-[16px]" /> */}
+          <div className="flex flex-col gap-[8px]">
+            <div className="flex items-center justify-between">
+              <div>
+                <p className="text-[12px] text-[#CCCCCC]">Yield source</p>
                 <p className="text-[16px] text-[#E4E4E4] font-normal">
-                  {collateralToken.info.underlyingCollateral}
+                  {collateralToken.info.yieldSource}
                 </p>
+              </div>
+              <div>
+                <p className="text-[12px] text-right text-[#CCCCCC]">
+                  Underlying collateral
+                </p>
+                <div className="flex text-right justify-end items-center gap-[2px]">
+                  {/* <img src={underlyingCollatateralIcon} alt="" className="w-[16px]" /> */}
+                  <p className="text-[16px] text-[#E4E4E4] font-normal">
+                    {collateralToken.info.underlyingCollateral}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <div>
+                <p className="text-[12px] text-[#CCCCCC]">Max Leverage</p>
+                <p className="text-[16px] text-[#E4E4E4] font-normal">
+                  {`< $${formatNumber(
+                    BigNumber.max(
+                      0,
+                      new BigNumber(collateralToken?.liquidityAssetsUsd)
+                        .dividedBy(BigNumber(9.1).minus(1))
+                        .minus(1000)
+                    )
+                  )}`}
+                </p>
+              </div>
+              <div>
+                <p className="text-[12px] text-right text-[#CCCCCC]">
+                  Available to Borrow
+                </p>
+                <div className="flex text-right justify-end items-center gap-[2px]">
+                  <p className="text-[16px] text-[#E4E4E4] font-normal">
+                    {`$${formatNumber(collateralToken.liquidityAssetsUsd)}`}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
